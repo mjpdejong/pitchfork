a80ec0d22bc45911e17baab603bc110eb3623cc7
Mengjuei <mjhsieh@gmail.com>
2016-06-24 00:14:29 -0700

    swig constantly lost the checksum

--- bin/travisbuild.sh
+++ bin/travisbuild.sh
@@ -2,7 +2,7 @@
 export PATH=/usr/lib/jvm/java-8-oracle/jre/bin:$PATH
 rm -rf $HOME/local.pip/wheel
 mkdir -p $HOME/distfiles
-# rm -rf $HOME/distfiles/libpng
+rm -rf $HOME/distfiles/swig
 test -e $HOME/distfiles/hdf5-1.8.13-linux-x86_64-shared.tar.gz \
 || curl -s -L https://www.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8.13/bin/linux-x86_64/hdf5-1.8.13-linux-x86_64-shared.tar.gz \
         -o $HOME/distfiles/hdf5-1.8.13-linux-x86_64-shared.tar.gz
